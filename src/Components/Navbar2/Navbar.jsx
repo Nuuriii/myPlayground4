048fb774b579f77f2a5506c715a9bd38729a17af
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-29 10:49:21 +0700

    feat: add new component

--- src/Components/Navbar2/Navbar.jsx
+++ src/Components/Navbar2/Navbar.jsx
@@ -1,5 +1,37 @@
-import React from "react";
-
 export const Navbar = () => {
-   return <div>Navbar</div>;
+   return (
+      <nav className='navigation'>
+         <a href='/' className='brand-name'>
+            Macrosoft
+         </a>
+         <button className='hamburger'>
+            {/* icon from heroicons.com */}
+            <svg
+               xmlns='http://www.w3.org/2000/svg'
+               className='h-5 w-5'
+               viewBox='0 0 20 20'
+               fill='white'
+            >
+               <path
+                  fillRule='evenodd'
+                  d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z'
+                  clipRule='evenodd'
+               />
+            </svg>
+         </button>
+         <div className='navigation-menu'>
+            <ul>
+               <li>
+                  <a href='/Home'>Home</a>
+               </li>
+               <li>
+                  <a href='/About'>About</a>
+               </li>
+               <li>
+                  <a href='/Contact'>Contact</a>
+               </li>
+            </ul>
+         </div>
+      </nav>
+   );
 };
